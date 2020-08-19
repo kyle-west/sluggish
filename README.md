2d5a642f5e67168f7d6e95d7541cc0e8d3570733
Kyle West <kyle@familysearch.org>
2020-08-19 10:20:20 -0600

    [styles] improve UX and update readme

--- README.md
+++ README.md
@@ -1,45 +1,38 @@
 # sluggish
 
-**Clone and run for a quick way to see Electron in action.**
+Compare two Slugs in Heroku.
 
-This is a minimal Electron application based on the [Quick Start Guide](https://electronjs.org/docs/tutorial/quick-start) within the Electron documentation.
+_This app is named what it is because it analyzes heroku slugs and it is very slow._
 
-**Use this app along with the [Electron API Demos](https://electronjs.org/#get-started) app for API code examples to help you get started.**
+![Screenshot](./sluggish.png)
 
-A basic Electron application needs just these files:
+## Setup and Install
 
-- `package.json` - Points to the app's main file and lists its details and dependencies.
-- `main.js` - Starts the app and creates a browser window to render HTML. This is the app's **main process**.
-- `index.html` - A web page to render. This is the app's **renderer process**.
+### Prerequisites
 
-You can learn more about each of these components within the [Quick Start Guide](https://electronjs.org/docs/tutorial/quick-start).
+This app relies on the `heroku` CLI and the `heroku-slugs` plugin. **You must be logged in for this tool to work**
 
-## To Use
+On MacOS:
 
-To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
-
-```bash
-# Clone this repository
-git clone https://github.com/kyle-west/sluggish
-# Go into the repository
-cd sluggish
-# Install dependencies
-npm install
-# Run the app
-npm start
+```sh
+brew tap heroku/brew && brew install heroku;
+heroku login;
 ```
 
-Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
+Then:
 
-## Resources for Learning Electron
+```sh
+heroku plugins:install heroku-slugs
+```
 
-- [electronjs.org/docs](https://electronjs.org/docs) - all of Electron's documentation
-- [electronjs.org/community#boilerplates](https://electronjs.org/community#boilerplates) - sample starter apps created by the community
-- [kyle-west/sluggish](https://github.com/kyle-west/sluggish) - a very basic starter Electron app
-- [electron/simple-samples](https://github.com/electron/simple-samples) - small applications with ideas for taking them further
-- [electron/electron-api-demos](https://github.com/electron/electron-api-demos) - an Electron app that teaches you how to use Electron
-- [hokein/electron-sample-apps](https://github.com/hokein/electron-sample-apps) - small demo apps for the various Electron APIs
+### Running this app
 
-## License
+_Disclaimer:_ Right now this product is a work in progress, so I have decided not to publish it as a full blown application. You can still install and run it, however, as you would any NPM application.
 
-[CC0 1.0 (Public Domain)](LICENSE.md)
+```
+git clone https://github.com/kyle-west/sluggish;
+cd sluggish;
+
+npm install;
+npm start;
+```
--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const slugs = require('./lib/heroku-slugs')
 
 
-const stamp = (selector, text, inner = 'innerHTML') => {
+const stamp = async (selector, text = '', inner = 'innerHTML') => {
   const element = document.querySelector(selector)
   if (element) {
     element[inner] = text
@@ -12,24 +12,36 @@ const stamp = (selector, text, inner = 'innerHTML') => {
 
 const beenTouched = (selector) => document.querySelector(selector).getAttribute('untouched') === null
 
-function changePage (page) {
+async function changePage (page, title, onBackClick) {
   [...document.querySelectorAll('[is-page-level]')].forEach(p => p.classList.add('hidden'));
   document.querySelector(page).classList.remove('hidden')
+
+  if (title) {
+    stamp('#instructions', title)
+  } 
+  if (onBackClick) {
+    const btn = document.createElement('button');
+    btn.onclick = onBackClick;
+    btn.innerHTML = '⬅ Back';
+    (await stamp('#back-button-wrapper')).appendChild(btn)
+  } else {
+    stamp('#back-button-wrapper')
+  } 
 }
 
 async function renderDiff(app, first, second) {
-  changePage('#hash-diff')
-  stamp('#hash-diff', `Download complete. Computing diff...`)
+  await changePage('#hash-diff', `Comparing: ${app} ${second.version} to ${first.version}`, () => renderAppSlugs(app))
+  await stamp('#hash-diff', `Download complete. Computing diff...`)
   const diff = await slugs.diff(app, first, second)
-  stamp('#hash-diff', diff)
+  await stamp('#hash-diff', diff)
 }
 
 async function renderAppSlugs(app) {
-  changePage('#slug-list')
-  stamp('#slug-list', 'Loading...')
+  await changePage('#slug-list', `Pick two slugs to compare in ${app}`, () => renderAppList())
+  await stamp('#slug-list', 'Loading...')
   try {
     const slugList = await slugs.getAll(app)
-    stamp('#slug-list', `
+    await stamp('#slug-list', `
       <button select-diff-slugs>Ready to compare</button>
       <table>
         <tr><th>Select 2</th><th>Version</th><th>Hash</th></tr>
@@ -43,7 +55,7 @@ async function renderAppSlugs(app) {
       const firstDL = slugs.downloadSlug(app, first.hash, (...args) => console.log('first', ...args))
       const secondDL = slugs.downloadSlug(app, second.hash, (...args) => console.log('second', ...args))
 
-      stamp('#slug-list', `
+      await stamp('#slug-list', `
         Downloading ${app}: ${first.version} and ${second.version}. This may take several minutes...
       `);
 
@@ -54,18 +66,18 @@ async function renderAppSlugs(app) {
       renderDiff(app, first, second)
     }
   } catch (err) {
-    stamp('#slug-list', `<pre>${err.message}</pre>`)
+    await stamp('#slug-list', `<pre>${err.message}</pre>`)
   }
 }
 
 async function renderAppList () {
   const appListId = '#app-list'
-  changePage(appListId)
+  await changePage(appListId, 'Click on an app to inspect')
   if (!beenTouched(appListId)) {
     const allApps = await slugs.fetchApps()
-    stamp(appListId, allApps.map(app => {
+    await stamp(appListId, allApps.map(app => {
       if (typeof app === 'string') {
-        return `<li on-click-render-app">${app}</li>`
+        return `<li on-click-render-app>${app}</li>`
       } else {
         const { group, apps } = app
         return `<li><details><summary>${group}</summary><ul>${
