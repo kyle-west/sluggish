253df638547414167f11a0aee4ee1c44a6875bff
Kyle West <kyle-west@users.noreply.github.com>
2021-08-12 14:08:58 -0600

    [slugs] add search and diff folder tree (#1)
    Search by text for heroku apps
    slug diff changed files tree

--- preload.js
+++ preload.js
@@ -1,6 +1,17 @@
 const slugs = require('./lib/heroku-slugs')
 const { resolve } = require('path')
 
+function testSearch({ target: { value } }) {
+  const searchable = [...document.querySelectorAll('[searchable]')]
+  searchable.forEach(element => {
+    if (value && element.innerText.indexOf(value) === -1) {
+      element.classList.add('hidden')
+    } else {
+      element.classList.remove('hidden')
+    }
+  })
+}
+
 const delay = (time) => new Promise(r => setTimeout(() => r(), time))
 
 const stamp = async (selector, text = '', inner = 'innerHTML') => {
@@ -32,15 +43,57 @@ async function changePage (page, title, onBackClick) {
   } 
 }
 
+function idify(str) {
+  return str.replace(/\W/g, '_')
+}
+
+function buildFileTree (paths) {
+  const root = {}
+  paths.forEach(path => {
+    let node = root
+    path.split('/').forEach((part) => {
+      node[part] = node[part] || {}
+      node = node[part]
+    })
+  })
+  return root
+}
+
+function buildFileTreeDOM (tree = {}, path = '') {
+  const entries = Object.entries(tree)
+  if (entries.length === 0) {
+    return ''
+  }
+  return entries.map(([key, node]) => Object.keys(node).length ? (`
+    <details ${path === '' ? 'open' : ''}>
+      <summary>${key}</summary>
+      ${buildFileTreeDOM(node, path + '/' + key)}
+    </details>
+  `): (`
+    <a href="#${idify(path + '/' + key)}">${key}</a>
+  `)).join('')
+}
+
 async function renderDiff(app, first, second) {
   await changePage('#hash-diff', `Comparing: ${app} ${second.version} to ${first.version}`, () => renderAppSlugs(app))
   await stamp('#hash-diff', `<div class="spinner"></div> Download complete. Computing diff...`)
   const diff = await slugs.diff(app, first, second)
   const rootPath = resolve('.', 'slugs')
-  await stamp('#hash-diff', diff
+  await stamp('#hash-diff', 
+    '<div id="file-tree"></div>' + 
+    diff
     .replace(new RegExp(`${rootPath}/{${second.hash} → ${first.hash}}/${app}/app/`, 'g'), `{${second.version} → ${first.version}} :: `)
     .replace(new RegExp('<span class="d2h-tag d2h-moved d2h-moved-tag">RENAMED</span>', 'g'), '')
   )
+  
+  const filenames = [...document.querySelectorAll('.d2h-file-name')]
+  filenames.forEach(filename => filename.setAttribute('id', idify('/' + filename.innerText.replace(new RegExp(`{${second.version} → ${first.version}} :: `, 'g'), ''))))
+  const fileTree = buildFileTree(filenames.map(x => x.innerText.replace(new RegExp(`{${second.version} → ${first.version}} :: `, 'g'), '')))
+  window.fileTree = fileTree
+  console.log(fileTree)
+  
+  await stamp('#file-tree', 'Files changed:<br/>' + buildFileTreeDOM(fileTree) + '<br/><hr/><br/>');
+
 }
 
 async function renderAppSlugs(app, pullFromCache = true) {
@@ -84,19 +137,23 @@ async function renderAppList () {
   await changePage(appListId, 'Click on an app to inspect')
   if (!beenTouched(appListId)) {
     const allApps = await slugs.fetchApps()
-    await stamp(appListId, allApps.map(app => {
-      if (typeof app === 'string') {
-        return `<li on-click-render-app>${app}</li>`
-      } else {
-        const { group, apps } = app
-        return `<li><details><summary>${group}</summary><ul>${
-          apps.map(a => `<li on-click-render-app>${a}</li>`).join('')
-        }</ul></details></li>`
-      }
-    }).join(''));
+    await stamp(appListId, 
+      '<label for="search">Search</label> <input id="search" name="search" type="text"/><br/><hr/><br/>'+
+      allApps.map(app => {
+        if (typeof app === 'string') {
+          return `<li searchable on-click-render-app>${app}</li>`
+        } else {
+          const { group, apps } = app
+          return `<li searchable><details><summary>${group}</summary><ul>${
+            apps.map(a => `<li on-click-render-app>${a}</li>`).join('')
+          }</ul></details></li>`
+        }}).join(''));
     [...document.querySelectorAll('[on-click-render-app]')].forEach(element => {
       element.onclick = () => renderAppSlugs(element.innerText)
-    })
+    });
+    const search = document.getElementById('search')
+    search.oninput = testSearch
+    search.focus()
   }
 }
 
